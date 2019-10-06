160da54facb8ee2b6943b44ad897c816edbdc75a
allison cortez <allizetorc@gmail.com>
2019-10-06 23:52:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -226,7 +226,7 @@ def most_points_scored
   game_hash.each do |place, team|
     team[:players].each do |player|
       if player[:points] > most_points
-        most_points = player[:points]
+        most_points = player[:player_name]
       end
     end
   end
