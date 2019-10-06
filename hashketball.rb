03b384dea73c0922e1f57859e87261077d3e40c1
allison cortez <allizetorc@gmail.com>
2019-10-06 23:29:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -437,23 +437,23 @@ end
 #   new_arr
 # end
 
-def player_numbers(team_name)
-  #returns arrray of jersey numbers for that team
+# def player_numbers(team_name)
+#   #returns arrray of jersey numbers for that team
   
-  nums = []
-  game_hash.each do |place, team|
-    if team[:team_name] == team_name
-    team.each do |attributes, data|
-      if attributes == :players
-        data.each do |player|
-          nums << player[:number]
-        end
-      end  
-    end
-  end  
- end 
- nums
-end
+#   nums = []
+#   game_hash.each do |place, team|
+#     if team[:team_name] == team_name
+#     team.each do |attributes, data|
+#       if attributes == :players
+#         data.each do |player|
+#           nums << player[:number]
+#         end
+#       end  
+#     end
+#   end  
+# end 
+# nums
+# end
 
 # def player_stats(players_name)
 #   new_hash = {}
